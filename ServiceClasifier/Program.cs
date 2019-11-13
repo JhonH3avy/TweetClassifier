1ac95489af15ec0f4aa7493522b8d88642bd24c4
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2019-11-13 16:24:21 -0500

    Load model into ML app

--- ServiceClasifier/Program.cs
+++ ServiceClasifier/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using Microsoft.ML;
+using ServiceClasifier.Models;
 
 namespace ServiceClasifier
 {
@@ -13,14 +14,17 @@ namespace ServiceClasifier
         private static string _modelPath => Path.Combine(_appPath, "..", "..", "..", "Models", "model.zip");
 
         private static MLContext _mlContext;
-        private static PredictionEngine<GitHubIssue, IssuePrediction> _predEngine;
+        private static PredictionEngine<Tweets, TweetCategoryPrediction> _predEngine;
         private static ITransformer _trainedModel;
         static IDataView _trainingDataView;
 
         static void Main(string[] args)
         {
             _mlContext = new MLContext(seed: 0);
-            _trainingDataView = _mlContext.Data.LoadFromTextFile<GitHubIssue>(_trainDataPath, hasHeader: true);
+            using (var contex = new TweetDataWarehouseContext())
+            {
+                _trainingDataView = _mlContext.Data.LoadFromEnumerable(contex.Tweets.ToList());
+            }
             var pipeline = ProcessData();
             var trainingPipeline = BuildAndTrainModel(_trainingDataView, pipeline);
             Evaluate(_trainingDataView.Schema);
@@ -30,10 +34,9 @@ namespace ServiceClasifier
 
         public static IEstimator<ITransformer> ProcessData()
         {
-            var pipeline = _mlContext.Transforms.Conversion.MapValueToKey(inputColumnName: "Area", outputColumnName: "Label")
-                .Append(_mlContext.Transforms.Text.FeaturizeText(inputColumnName: "Title", outputColumnName: "TitleFeaturized"))
-                .Append(_mlContext.Transforms.Text.FeaturizeText(inputColumnName: "Description", outputColumnName: "DescriptionFeaturized"))
-                .Append(_mlContext.Transforms.Concatenate("Features", "TitleFeaturized", "DescriptionFeaturized"))
+            var pipeline = _mlContext.Transforms.Conversion.MapValueToKey(inputColumnName: "Category", outputColumnName: "Label")
+                .Append(_mlContext.Transforms.Text.FeaturizeText(inputColumnName: "Text", outputColumnName: "TextFeaturized"))
+                .Append(_mlContext.Transforms.Concatenate("Features", "TextFeaturized"))
                 .AppendCacheCheckpoint(_mlContext);
             return pipeline;
         }
@@ -41,45 +44,44 @@ namespace ServiceClasifier
         public static IEstimator<ITransformer> BuildAndTrainModel(IDataView trainingDataView, IEstimator<ITransformer> pipeline)
         {
             var trainingPipeline = pipeline.Append(_mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy("Label", "Features"))
-                .Append(_mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+                .Append(_mlContext.Transforms.Conversion.MapKeyToValue("PredictedCategory"));
             _trainedModel = trainingPipeline.Fit(trainingDataView);
-            _predEngine = _mlContext.Model.CreatePredictionEngine<GitHubIssue, IssuePrediction>(_trainedModel);
-            GitHubIssue issue = new GitHubIssue()
+            _predEngine = _mlContext.Model.CreatePredictionEngine<Tweets, TweetCategoryPrediction>(_trainedModel);
+            var tweet = new Tweets
             {
-                Title = "WebSockets communication is slow in my machine",
-                Description = "The WebSockets communication used under the covers by SignalR looks like is going slow in my development machine.."
+                Text = "Avianca es una aerolinea muy buena"
             };
-            var prediction = _predEngine.Predict(issue);
-            Console.WriteLine($"=============== Single Prediction just-trained-model - Result: {prediction.Area} ===============");
+            var prediction = _predEngine.Predict(tweet);
+            Console.WriteLine($"=============== Single Prediction just-trained-model - Result: {prediction.Category} ===============");
             return trainingPipeline;
         }
 
         public static void Evaluate(DataViewSchema trainingDataViewSchema)
         {
-            var testDataView = _mlContext.Data.LoadFromTextFile<GitHubIssue>(_testDataPath,hasHeader: true);
-            var testMetrics = _mlContext.MulticlassClassification.Evaluate(_trainedModel.Transform(testDataView));
-            Console.WriteLine($"*************************************************************************************************************");
-            Console.WriteLine($"*       Metrics for Multi-class Classification model - Test Data     ");
-            Console.WriteLine($"*------------------------------------------------------------------------------------------------------------");
-            Console.WriteLine($"*       MicroAccuracy:    {testMetrics.MicroAccuracy:0.###}");
-            Console.WriteLine($"*       MacroAccuracy:    {testMetrics.MacroAccuracy:0.###}");
-            Console.WriteLine($"*       LogLoss:          {testMetrics.LogLoss:#.###}");
-            Console.WriteLine($"*       LogLossReduction: {testMetrics.LogLossReduction:#.###}");
-            Console.WriteLine($"*************************************************************************************************************");
+            //var testDataView = _mlContext.Data.LoadFromTextFile<GitHubIssue>(_testDataPath,hasHeader: true);
+            //var testMetrics = _mlContext.MulticlassClassification.Evaluate(_trainedModel.Transform(testDataView));
+            //Console.WriteLine($"*************************************************************************************************************");
+            //Console.WriteLine($"*       Metrics for Multi-class Classification model - Test Data     ");
+            //Console.WriteLine($"*------------------------------------------------------------------------------------------------------------");
+            //Console.WriteLine($"*       MicroAccuracy:    {testMetrics.MicroAccuracy:0.###}");
+            //Console.WriteLine($"*       MacroAccuracy:    {testMetrics.MacroAccuracy:0.###}");
+            //Console.WriteLine($"*       LogLoss:          {testMetrics.LogLoss:#.###}");
+            //Console.WriteLine($"*       LogLossReduction: {testMetrics.LogLossReduction:#.###}");
+            //Console.WriteLine($"*************************************************************************************************************");
         }
 
         private static void SaveModelAsFile(MLContext mlContext, DataViewSchema trainingDataViewSchema, ITransformer model)
         {
-            mlContext.Model.Save(model, trainingDataViewSchema, _modelPath);
+            //mlContext.Model.Save(model, trainingDataViewSchema, _modelPath);
         }
 
         private static void PredictIssue()
         {
             ITransformer loadedModel = _mlContext.Model.Load(_modelPath, out var modelInputSchema);
-            GitHubIssue singleIssue = new GitHubIssue() { Title = "Entity Framework crashes", Description = "When connecting to the database, EF is crashing" };
-            _predEngine = _mlContext.Model.CreatePredictionEngine<GitHubIssue, IssuePrediction>(loadedModel);
-            var prediction = _predEngine.Predict(singleIssue);
-            Console.WriteLine($"=============== Single Prediction - Result: {prediction.Area} ===============");
+            var singleTweet = new Tweets() { Text = "Un avion de avianca se estrello" };
+            _predEngine = _mlContext.Model.CreatePredictionEngine<Tweets, TweetCategoryPrediction>(loadedModel);
+            var prediction = _predEngine.Predict(singleTweet);
+            Console.WriteLine($"=============== Single Prediction - Result: {prediction.Category} ===============");
         }
     }
 }
